0b16346fd1a53c57f65a51d49e7e59ede4ccaf33
Hassam <haxxevony@gmail.com>
2025-10-08 01:34:25 +0100

    Added fast homepage view for root URL

--- run_once.py
+++ run_once.py
@@ -5,6 +5,16 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cars_platform.settings")
 django.setup()
 
 from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
+# Run migrations
 call_command("migrate")
-call_command("createsuperuser", interactive=True)
+
+# Create superuser if not exists
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="Haxxy",
+        email="haxxevony@gmail.com",
+        password="NewSecurePassword123"
+    )
